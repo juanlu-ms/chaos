5133c72b0ec31019395a9d3dcaa69f17cfd36909
juanlu-ms <juanlums11@gmail.com>
2026-03-20 16:24:53 +0000

    Implement TransparentStringHash for custom unordered_map key handling in Parameters

--- orchestrator/include/manifests/Manifest.hpp
+++ orchestrator/include/manifests/Manifest.hpp
@@ -6,7 +6,21 @@
 
 namespace chaos::orchestrator::manifests {
 
-using Parameters = std::unordered_map<std::string, std::string>;
+struct TransparentStringHash {
+    using is_transparent = void;
+
+    [[nodiscard]] std::size_t operator()(std::string_view key) const noexcept {
+        return std::hash<std::string_view>{}(key);
+    }
+
+    [[nodiscard]] std::size_t operator()(const std::string& key) const noexcept {
+        return (*this)(std::string_view{key});
+    }
+
+    [[nodiscard]] std::size_t operator()(const char* key) const noexcept { return (*this)(std::string_view{key}); }
+};
+
+using Parameters = std::unordered_map<std::string, std::string, TransparentStringHash, std::equal_to<>>;
 
 struct Target {
     std::string type;

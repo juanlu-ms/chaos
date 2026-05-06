ab86f409320240c91a843702a75d068813c65c9f
juanlu-ms <juanlums11@gmail.com>
2026-05-06 22:38:29 +0000

    Refactor container validation tests to improve readability and consistency

--- orchestrator/tests/smoke/ContainerValidationTest.cpp
+++ orchestrator/tests/smoke/ContainerValidationTest.cpp
@@ -7,9 +7,7 @@ using chaos::orchestrator::containers::createContainerEngine;
 
 class ContainerValidationTest : public ::testing::Test {
 protected:
-    void SetUp() override {
-        engine_ = createContainerEngine();
-    }
+    void SetUp() override { engine_ = createContainerEngine(); }
 
     std::shared_ptr<chaos::orchestrator::containers::IContainerEngine> engine_;
 };
@@ -31,37 +29,37 @@ TEST_F(ContainerValidationTest, RemoveContainerThrowsOnEmptyId) {
 }
 
 TEST_F(ContainerValidationTest, GetStatusThrowsOnEmptyId) {
-    EXPECT_THROW(engine_->getStatus(""), std::invalid_argument);
+    EXPECT_THROW((void)engine_->getStatus(""), std::invalid_argument);
 }
 
 TEST_F(ContainerValidationTest, ExecThrowsOnEmptyContainerId) {
-    EXPECT_THROW(engine_->exec("", "echo hello"), std::invalid_argument);
+    EXPECT_THROW((void)engine_->exec("", "echo hello"), std::invalid_argument);
 }
 
 TEST_F(ContainerValidationTest, ExecThrowsOnEmptyCommand) {
-    EXPECT_THROW(engine_->exec("some-id", ""), std::invalid_argument);
+    EXPECT_THROW((void)engine_->exec("some-id", ""), std::invalid_argument);
 }
 
 TEST_F(ContainerValidationTest, GetLogsThrowsOnEmptyContainerId) {
-    EXPECT_THROW(engine_->getLogs(""), std::invalid_argument);
+    EXPECT_THROW((void)engine_->getLogs(""), std::invalid_argument);
 }
 
 TEST_F(ContainerValidationTest, UpdateMemoryLimitThrowsOnEmptyId) {
-    EXPECT_THROW(engine_->updateMemoryLimit("", 1024), std::invalid_argument);
+    EXPECT_THROW((void)engine_->updateMemoryLimit("", 1024), std::invalid_argument);
 }
 
 TEST_F(ContainerValidationTest, UpdateCpuQuotaThrowsOnEmptyId) {
-    EXPECT_THROW(engine_->updateCpuQuota("", 50000, 100000), std::invalid_argument);
+    EXPECT_THROW((void)engine_->updateCpuQuota("", 50000, 100000), std::invalid_argument);
 }
 
 TEST_F(ContainerValidationTest, GetContainerMemoryUsageThrowsOnEmptyId) {
-    EXPECT_THROW(engine_->getContainerMemoryUsage(""), std::invalid_argument);
+    EXPECT_THROW((void)engine_->getContainerMemoryUsage(""), std::invalid_argument);
 }
 
 TEST_F(ContainerValidationTest, GetContainerCpuUsageThrowsOnEmptyId) {
-    EXPECT_THROW(engine_->getContainerCpuUsage(""), std::invalid_argument);
+    EXPECT_THROW((void)engine_->getContainerCpuUsage(""), std::invalid_argument);
 }
 
 TEST_F(ContainerValidationTest, GetContainerIpThrowsOnEmptyId) {
-    EXPECT_THROW(engine_->getContainerIp(""), std::invalid_argument);
+    EXPECT_THROW((void)engine_->getContainerIp(""), std::invalid_argument);
 }

232bd10a22aef9c02caef8d7034a779c6a99c8d8
juanlu-ms <juanlums11@gmail.com>
2026-05-08 13:52:08 +0000

    Implement theme system with CSS variables and FTXUI color constants

---
+++ orchestrator/include/ui/Theme.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <ftxui/dom/elements.hpp>
+namespace chaos::orchestrator::ui::theme {
+inline constexpr auto kBgPrimary = ftxui::Color::RGB(0x1a, 0x14, 0x10);
+inline constexpr auto kBgSurface = ftxui::Color::RGB(0x24, 0x1e, 0x18);
+inline constexpr auto kBorder = ftxui::Color::RGB(0x3d, 0x32, 0x28);
+inline constexpr auto kTextPrimary = ftxui::Color::RGB(0xe8, 0xdd, 0xd0);
+inline constexpr auto kAccent = ftxui::Color::RGB(0xe6, 0xa8, 0x17);
+inline constexpr auto kSuccess = ftxui::Color::RGB(0x4c, 0xaf, 0x50);
+inline constexpr auto kError = ftxui::Color::RGB(0xef, 0x53, 0x50);
+}  // namespace chaos::orchestrator::ui::theme

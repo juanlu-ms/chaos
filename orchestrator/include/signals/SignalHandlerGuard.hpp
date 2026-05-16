10d67ab5dd28f8bfbf6e796da5adedd7eb52b6f1
Juanlu <juanlums11@gmail.com>
2026-05-16 21:21:52 +0000

    Format refactor

--- orchestrator/include/signals/SignalHandlerGuard.hpp
+++ orchestrator/include/signals/SignalHandlerGuard.hpp
@@ -47,7 +47,7 @@ public:
     [[nodiscard]] bool valid() const;
 
 private:
-    struct sigaction previous_{};
+    struct sigaction previous_ {};
     bool previous_valid_{false};
     int pipe_read_{-1};
     int pipe_write_{-1};
